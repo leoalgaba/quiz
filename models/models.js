71d28f56964126c87952e73b6883cd9317924dad
leoalgaba <leo_algaba@hotmail.com>
2016-04-21 19:37:55 +0200

    sqlite_sequelize

---
+++ models/models.js
@@ -0,0 +1,29 @@
+var path = require('path'),
+	Sequelize = require('sequelize');
+
+// Usar BBDD SQLite (ahora secuelize va en minusculas)
+var sequelize = new Sequelize(null, null, null,
+						{
+							dialect:"sqlite", storage: "quiz.sqlite"
+						}
+					);
+
+// Importar la definicion de la tabla Quiz en quiz.js
+var Quiz = sequelize.import(path.join(__dirname,'quiz'));
+exports.Quiz=Quiz; //exportar definicion de tabla Quiz
+
+// sequelize.sync() crea e inicializa tabla de preguntas en la bbdd
+sequelize.sync().success(function () {
+	// success(..) ejecuta el manejador una vez creada la tabla
+	Quiz.count().success (function (count) {
+		if (count ===0) { //la tabla se inicializa solo si esta vacia
+			Quiz.create ({
+				pregunta: 'Capital de Italia',
+				respuesta: 'Roma'
+			})
+			.success(function () {
+				console.log('Base de datos inicializada');
+			});
+		};
+	});
+});
